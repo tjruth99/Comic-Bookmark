66acc09bc79666e2de3facac7fe323fd4e1834dd
Tyler Ruth <tyler1hero@sbcglobal.net>
2019-04-21 22:34:32 -0400

    Sign In and Log In are now within the Profile page when the user is not signed in

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,5 +1,10 @@
 import React from 'react';
 import ProfileList from './components/ProfileList'
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+import LogIn from "./LogIn";
+import SignUp from "./SignUp";
+
 import * as firebase from 'firebase';
 
 class Profile extends React.Component {
@@ -15,6 +20,7 @@ class Profile extends React.Component {
   }
 
   componentDidMount(){
+    console.log("(" + this.state.username + ")");
     this.setState({
       username: localStorage.getItem('_username'),
       email: localStorage.getItem('_userEmail')
@@ -39,28 +45,64 @@ class Profile extends React.Component {
         alert("Error when signing out: " + error.message)
         console.log(error.message);
       });
+
+    window.location.reload();
   }
 
   render () {
-    return (
-      <div>
-        <h1>Profile</h1>
-        <h2>Username: {this.state.username}</h2>
-        <h2>Email: {this.state.email}</h2>
-        <p>
-          Display a list of the stories the user is currently reading and have buttons for the user to press to continue or go back on issues
+    console.log("(" + this.state.username + ")");
+
+    if( this.state.email == null ){
+      return (
+        <div>
+          <h2>Please Sign In</h2>
           <br />
-          Display the next issue of the story in smaller, dimmed text
-        </p>
-        <ProfileList />
-        <button
-          type="button"
-          onClick={this.signOut}
-        >
-          Sign Out
-        </button>
-      </div>
-    );
+          <Router>
+            <Link to="/profile/login">
+              <button
+                name="loginButton"
+                type="button"
+                className="authButton"
+              >
+                Login
+              </button>
+            </Link>
+            <div className="divider" />
+            <Link to="/profile/signup">
+              <button
+                name="signupButton"
+                type="button"
+                className="authButton"
+              >
+                Sign-Up
+              </button>
+            </Link>
+            <Route path="/profile/login" component={LogIn} />
+            <Route path="/profile/signup" component={SignUp} />
+          </ Router>
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <h1>Profile</h1>
+          <h2>Username: {this.state.username}</h2>
+          <h2>Email: {this.state.email}</h2>
+          <p>
+            Display a list of the stories the user is currently reading and have buttons for the user to press to continue or go back on issues
+            <br />
+            Display the next issue of the story in smaller, dimmed text
+          </p>
+          <ProfileList />
+          <button
+            type="button"
+            onClick={this.signOut}
+          >
+            Sign Out
+          </button>
+        </div>
+      );
+    }
   }
 }
 
