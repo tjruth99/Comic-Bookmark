ce2172290feb0822352358bfc092bc0faa85e083
Owen Shen <shenowen36@gmail.com>
2019-04-23 22:35:35 -0400

    Reduce errors, still looking to solve function err

--- functions/index.js
+++ functions/index.js
@@ -37,72 +37,74 @@ res.status(200).send('smile');
 //    username:  user given name for their account
 // Add a new document to the users collection with a key of userID and a field username
 exports.addUserToDatabase = functions.https.onRequest((req, res) => {
-  cors(req, res, () => {
-    const userID = req.body.userID;
-    const user = req.body.username;
+  console.log("Test addd");
+  res.send("Test add");
+  //   cors(req, res, () => {
+//     const userID = req.body.userID;
+//     const user = req.body.username;
 
-    if(userID == null){
-      console.log("addUserToDatabase userID null");
-    }
+//     if(userID == null){
+//       console.log("addUserToDatabase userID null");
+//     }
 
-    // Add a new document in usernames collection
-    db.collection("usernames").doc(user).set({
-      userID: userID
-    })
-    .catch(function(error) {
-      console.error("Error writing document: ", error);
-    });
+//     // Add a new document in usernames collection
+//     db.collection("usernames").doc(user).set({
+//       userID: userID
+//     })
+//     .catch(function(error) {
+//       console.error("Error writing document: ", error);
+//     });
 
-    // Add a new document to users collection
-    db.collection("users").doc(userID).set({
-      username: user,
-    })
-    .catch(function(error) {
-      console.error("Error writing document: ", error);
-    });
+//     // Add a new document to users collection
+//     db.collection("users").doc(userID).set({
+//       username: user,
+//     })
+//     .catch(function(error) {
+//       console.error("Error writing document: ", error);
+//     });
 
-    // Add subCollection to users collection
-    db.collection("users").doc(userID).collection("Reading");
+//     // Add subCollection to users collection
+//     db.collection("users").doc(userID).collection("Reading");
 
-    console.log('User added to username and users db');
-  })
-});
+//     console.log('User added to username and users db');
+//   })
+// });
 
-// Function: startReading
-// parameters:
-//    userID: random string for given user
-//    seriesName: name of the series to add to the user's document
-// Adds a new map to the array "Reading" in the given userID document
-// The field should be seriesName and the value should be 1
-exports.startReading = functions.https.onRequest((req, res) => {
-  const userID = req.body.userID;
-  const seriesName = req.body.seriesName;
+// // Function: startReading
+// // parameters:
+// //    userID: random string for given user
+// //    seriesName: name of the series to add to the user's document
+// // Adds a new map to the array "Reading" in the given userID document
+// // The field should be seriesName and the value should be 1
+// exports.startReading = functions.https.onRequest((req, res) => {
+//   const userID = req.body.userID;
+//   const seriesName = req.body.seriesName;
 
-  db.collection("users").doc(userID).collection("Reading").doc(seriesName).set({
-    seriesName: seriesName,
-    currentIssue: 1
-  }).catch(function(error) {
-    console.error("Error writing into subCollection: ", error);
-  });
+//   db.collection("users").doc(userID).collection("Reading").doc(seriesName).set({
+//     seriesName: seriesName,
+//     currentIssue: 1
+//   }).catch(function(error) {
+//     console.error("Error writing into subCollection: ", error);
+//   });
 
-  console.log('Started series ' + seriesName);
-});
+//   console.log('Started series ' + seriesName);
+// });
 
-// Function: nextIssue
-// parameters:
-//    userID: random string for given user
-//    seriesName: name of the series to add to the user's document
-// Goes to "Reading" field in the user's document and increments the current issue for the given seriesName
-exports.nextIssue = functions.https.onRequest((req, res) => {
-  const userID = req.body.userID;
-  const seriesName = req.body.seriesName;
+// // Function: nextIssue
+// // parameters:
+// //    userID: random string for given user
+// //    seriesName: name of the series to add to the user's document
+// // Goes to "Reading" field in the user's document and increments the current issue for the given seriesName
+// exports.nextIssue = functions.https.onRequest((req, res) => {
+//   const userID = req.body.userID;
+//   const seriesName = req.body.seriesName;
 
-  db.collection("users").doc(userID).collection("Reading").doc(seriesName).update({
-    //try to increment issue
-    currentIssue: admin.firestore.FieldValue.increment(1)
-  }).catch(function(error) {
-    console.error("Error incrementing issue ", error);
-  });
+//   db.collection("users").doc(userID).collection("Reading").doc(seriesName).update({
+//     //try to increment issue
+//     currentIssue: admin.firestore.FieldValue.increment(1)
+//   }).catch(function(error) {
+//     console.error("Error incrementing issue ", error);
+//   });
 
 });
 
